ce9b21e8f56b3dd47e89139d54c8a830842b7933
houssame <houssamehajji@gmail.com>
2013-01-24 18:24:12 +0000

    dummy data
    Change-Id: I72a81290cc4daf928a305c736981c4e04b500767

---
+++ Image/src/js/dummyData.js
@@ -0,0 +1,86 @@
+Images = {
+		listImages : [ {
+			id : 0,
+			src : "",
+			story : "",
+			title : "",
+			ownerId : 0,
+			likes : {
+				numberLikes : "",
+				likersList : [ 11, 13 ]
+			},
+			comments : {
+				numberComments : "",
+				commentersList : [ 11, 13 ]
+			},
+			interestsList : []
+		}, {
+			id : 1,
+			src : "",
+			story : "",
+			title : "",
+			ownerId : 12,
+			likes : {
+				numberLikes : "",
+				likersList : [ 11, 13 ]
+ 			},
+ 			comments : {
+ 				numberComments : "",
+ 				commentersList : [ 11, 13 ]
+ 			},
+ 			interestsList : []
+		}, {
+			id : 2,
+			src : "",
+			story : "",
+			title : "",
+			ownerId : 12,
+			likes : {
+				numberLikes : "",
+				likersList : [ 11, 13 ]
+ 			},
+ 			comments : {
+ 				numberComments : "",
+ 				commentersList : [ 11, 13 ]
+ 			},
+ 			interestsList : []
+		}, {
+			id : 3,
+			src : "",
+			story : "",
+			title : "",
+			ownerId : 12,
+			likes : {
+				numberLikes : "",
+				likersList : [ 11, 13 ]
+			},
+			comments : {
+				numberComments : "",
+				commentersList : [ 11, 13 ]
+			},
+			interestsList : []
+		}]
+
+};
+
+users = {
+		listUsers : [ {
+			id : 0,
+			name : "",
+			portrait : "",
+			imagesList : [],
+			likedImagesList:[],
+		},{
+			id : 1,
+			name : "",
+			portrait : "",
+			imagesList : [],
+			likedImagesList:[],
+		},{
+			id : 2,
+			name : "",
+			portrait : "",
+			imagesList : [],
+			likedImagesList:[],
+		}]
+};
