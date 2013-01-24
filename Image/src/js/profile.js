2f62884d85e1d2beaf00b58628a776d7b808bbab
Younes Chahbi <younes.chahbi@sap.com>
2013-01-24 12:04:13 +0100

    first version
    Change-Id: I95ee30679fbb661823c46c39d2e93baba1d24c0a

---
+++ Image/src/js/profile.js
@@ -0,0 +1,12 @@
+function creatImageLike() {
+	return "<div style=\"width: 50%;margin-left:25% ;height: 33px;color: aqua;border-style: solid;border-color: red;-moz-border-radius: 100px / 160px; -webkit-border-radius: 100px / 160px; border-radius: 100px / 160px;\">"
+	
+	+"<img style=\"height:75%; position:relative; top: 12% ;left: 2% \" src=\"img/Koala1.jpg\">"
+	
+	+"<img style=\"height:75%; width:4%; position:relative; top: 12% ;margin-left : 46% \" src=\"img/like.jpg\">"
+	+"<label  style=\"height:75%; position:relative; top: 12% ;left: 50% \">"
+	+"</label>"
+	+"<button style=\"height:75%;width:4%; position:relative; background-image:url('./img/minusicone.png');margin-left: 90%;top: -75%\" >"
+	+"</button>"
+	+"</div>";
+}
